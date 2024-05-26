966c6eee5d09cd0c1f376d6fbc811c1646fd0cec
Anutama Developers <anutama.developers@gmail.com>
2024-05-26 08:58:37 -0400

    added mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,30 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName:"Pradeep", lastName:"Prabhu"}
+  const loggedIn = { firstName: "Pradeep", lastName: "Prabhu" };
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn}/>
+      <Sidebar user={loggedIn} />
       <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image src="./icons/logo.svg" width={30} height={30} alt="menu icon"></Image>
-            <div>
-              
-            </div>
+        <div className="root-layout">
+          <Image
+            src="./icons/logo.svg"
+            width={30}
+            height={30}
+            alt="logo"
+          ></Image>
+          <div>
+            <MobileNav user={loggedIn} />
           </div>
+        </div>
+        {children}
       </div>
-      {children}
     </main>
   );
 }
